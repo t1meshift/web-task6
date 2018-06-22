a9759b63cda2f16d55763eff0ded04d9505ba409
t1meshift <tarakanama@mail.ru>
2018-06-22 17:29:25 +1000

    Improve error render

--- routes/user.js
+++ routes/user.js
@@ -3,39 +3,39 @@ let router = express.Router();
 let mongo = require("mongodb").MongoClient;
 let config = require("../config");
 
-function renderError(response, errCode, errDescr) {
-    console.error(errDescr);
-    response.status(500);
-    response.render('user', {
+function renderError(response, errName, errDescr = null, errCode = 500) {
+    console.error(`Error "${errName}" invoked:`, errDescr);
+    response.status(500).render('error', {
         task: config.siteName,
-        err: errCode,
-        errDesc: errDescr,
+        message: errName,
+        error: {
+            stack: errDescr,
+            status: ""
+        }
     });
 }
 
 /* GET home page. */
 router.get('', function(req, res, next) {
-    res.status(403);
-    res.send("Nope c:");
+    renderError(res, "YOU SHALL NOT PASS!", "Nothing personal, just 403.", 403);
 });
 router.get('/:userName', function(req, res, next) {
     let userName = req.params.userName;
     mongo.connect(config.db.url, config.dbConnection, function(err, client) {
         if (err) {
-            renderError(res, 1, err.toString());
+            renderError(res, "Ошибка БД", err.toString());
         } else {
             const dataBase = client.db(config.db.name);
             const users = dataBase.collection("users");
             users.find({login: userName}).toArray(function(err, docs) {
                 if (err) {
-                    renderError(res, 1, err.toString());
+                    renderError(res, "Ошибка БД", err.toString());
                 } else if (docs.length !== 1) {
-                    renderError(res, 2, `Пользователь ${userName} не найден :с`);
+                    renderError(res, `Пользователь ${userName} не найден :с`, null, 404);
                 } else {
                     res.render('user', {
                         task: config.siteName,
-                        user: docs[0],
-                        err: 0,
+                        user: docs[0]
                     });
                 }
             });
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,15 @@ let router = express.Router();
 let mongo = require("mongodb").MongoClient;
 let config = require("../config");
 
-function renderError(response, errCode, errDescr) {
-    console.error(errDescr);
-    response.status(500);
-    response.render('users', {
+function renderError(response, errName, errDescr = null) {
+    console.error(`Error "${errName}" invoked:`, errDescr);
+    response.status(500).render('error', {
         task: config.siteName,
-        err: errCode,
-        errDesc: errDescr,
+        message: errName,
+        error: {
+            stack: errDescr,
+            status: ""
+        }
     });
 }
 
@@ -17,18 +19,17 @@ function renderError(response, errCode, errDescr) {
 router.get('/', function(req, res, next) {
     mongo.connect(config.db.url, config.dbConnection, function(err, client) {
         if (err) {
-            renderError(res, 1, err.toString());
+            renderError(res, "Ошибка БД", err.toString());
         } else {
             const dataBase = client.db(config.db.name);
             const users = dataBase.collection("users");
             users.find({login: {$ne: "admin"}}).sort({login: -1}).toArray(function(err, docs) {
                 if (err) {
-                    renderError(res, 1, err.toString());
+                    renderError(res, "Ошибка БД", err.toString());
                 } else {
                     res.render('users', {
                         task: config.siteName,
-                        users: docs,
-                        err: 0,
+                        users: docs
                     });
                 }
             });
